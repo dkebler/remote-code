34a04993462573df2db8cd7ccf295bcc0553b58c
David Kebler <d@kebler.net>
2018-11-17 22:05:56 -0800

    First Improvments from Fork
    Now uses node-config for startup from a config file (in my case a yaml)
    https://github.com/lorenwest/node-config
    
    Now works with a key-agent and also allows the text password access too via (awaiting testing)
    
    modified the cli and remotecode class so that one can use the config file from the cli or direct from a remotecode instance.  Thus the defaults get set in remotecode class constructor and no longer in the cli.
    
    Added a few other options
    registry:  url of custom registry that will be used on install
    key-agent: the environment variable holding the path to the agent socket,  there are a couple names tried by default
    
    here is a sample config file
    ```
    #commented keys indicate their default
    options:
      target: /opt/relays
      #install: 'yarn'
      #registry:    # used for grabbing from custom registry first
      #source: .
      #start: 'nodemon .'  # pm2 could be used as well
      #verbose: false
      ssh:
        host: relays.kebler.net
        # port: 22
        username: sysadmin
        # need one of these three.  If none given will try to find the keyagent at some typical names
        #keyfilePath:
        #agent:  # the environment variable name of your ssh agent socket, defaults are SSH_AUTH_SOCK and SSH_AGENT_SOCK
        #password:  # for non keyfile access
        # keepaliveInterval: 500
        # readyTimeout: 2000
    ```

--- cli.js
+++ cli.js
@@ -1,15 +1,16 @@
 #!/usr/bin/env node
-const path = require('path');
-const stream = require('stream');
-const url = require('ssh-url');
-const chalk = require('chalk');
-const meow = require('meow');
-const updateNotifier = require('update-notifier');
-const pkg = require('./package.json');
-const RemoteCode = require('./index');
+const path = require('path')
+const stream = require('stream')
+const url = require('ssh-url')
+const chalk = require('chalk')
+const meow = require('meow')
+const config = require('config')
+const updateNotifier = require('update-notifier')
+const pkg = require('./package.json')
+const RemoteCode = require('.')
 
-// check for new version
-updateNotifier({pkg}).notify();
+// Check for new version
+updateNotifier({pkg}).notify()
 
 const cli = meow(`
 	Usage
@@ -26,13 +27,16 @@ const cli = meow(`
 
 	Options
 		--identity-file, -i	SSH keyfile
+		--key-agent, -a	key agent socket path (usually found in SSH_AUTH_SOCK env variable)
 		--install, -I		installation / setup command [yarn]
 		--port, -p		Custom port [22]
 		--source, -s 		directory to synchronize (local) [CWD]
 		--start, -S		command to start on remote (should implement a file watcher) [nodemon .]
 		--target, -t 		remote location to sync to [~/remote-sync]
 		--user, -u		SSH username
-		--verbose, -v 		log all the things
+		--registry, -r  url of alternative (local) registry for package install
+		--verbose, -v 	log all the things
+		--config, -c 		specify a config file path (looks for ./config/default.*)
 
 	Examples
 		$ remote-code user@192.168.0.4
@@ -40,83 +44,94 @@ const cli = meow(`
 		$ remote-code -i ~/.ssh/id_rsa pi@192.168.0.4 --source ~/myProject --target ~/myProject
 		$ remote-code -i ~/.ssh/id_rsa pi@192.168.0.4 -S 'sudo \`which node\` johnny5' -I "npm install"
 `, {
-	alias: {
-		p: 'port',
-		i: 'identity-file',
-		I: 'install',
-		u: 'user',
-		P: 'password', /* TODO		--password, -P		SSH password (not supported) */
-		s: 'source',
-		S: 'start',
-		t: 'target',
-		v: 'verbose'
+	flags: {
+		keyAgent: {alias: 'k'},
+		port: {alias: 'p'},
+		identityFile: {alias: 'i'},
+		user: {alias: 'u'},
+		password: {alias: 'P'},
+		start: {alias: 'S'},
+		target: {alias: 't'},
+		source: {alias: 's'},
+		verbose: {alias: 'v'},
+		install: {alias: 'I'},
+		registry: {alias: 'r'},
+		config: {alias: 'c'}
 	}
-});
+})
 
-// show help if no host is supplied
-if (cli.input.length === 0) {
-	console.log(cli.help);
-	process.exit();
-}
-// parse argument for user/host
-const parts = url.parse(cli.input[0]);
-const options = {
-	ssh: {
-		host: parts.hostname,
-		port: cli.flags.port || 22,
-		username: cli.flags.user || parts.user,
-		keyfilePath: cli.flags.identityFile,
-		password: cli.flags.password,
-		keepaliveInterval: 500,
-		readyTimeout: 2000
-	},
-	install: cli.flags.install || 'yarn',
-	source: path.normalize(cli.flags.source || process.cwd()),
-	start: cli.flags.start || 'nodemon .',
-	target: cli.flags.target || '~/remote-sync',
-	verbose: cli.flags.verbose
-};
+// Show help if no host is supplied
+// const options = config.get('options.ssh.host')
 
-// check for missing options
+let options = config.get('options')
 if (!options.ssh.host) {
-	console.log('Please provide a valid host');
-	process.exit();
+	if (cli.input.length === 0) {
+		console.log(cli.help)
+		process.exit()
+	}
+	// Parse argument for user/host
+	const parts = url.parse(cli.input[0])
+	options = {
+		ssh: {
+			host: parts.hostname,
+			port: cli.flags.port || 22,
+			username: cli.flags.user || parts.user,
+			keyfilePath: cli.flags.identityFile,
+			agent: cli.flags.keyAgent,
+			password: cli.flags.password,
+			keepaliveInterval: 500,
+			readyTimeout: 2000
+		},
+		install: cli.flags.install || 'yarn',
+		registry: cli.flags.registry,
+		config: cli.flags.config,
+		source: path.normalize(cli.flags.source || process.cwd()),
+		start: cli.flags.start || 'nodemon .',
+		target: cli.flags.target || '~/remote-sync',
+		verbose: cli.flags.verbose
+	}
+}
+
+// Check for missing options
+if (!options.ssh.host) {
+	console.log('Please provide a valid host')
+	process.exit()
 }
 if (!options.ssh.username) {
-	console.log('Please provide a valid username');
-	process.exit();
+	console.log('Please provide a valid username')
+	process.exit()
 }
 
 // verbose stream processing
 const verbOut = new stream.Transform({
-	transform: function (chunk, enc, cb) {
-		cb(null, chalk.dim(chunk));
+	transform(chunk, enc, cb) {
+		cb(null, chalk.dim(chunk))
 	}
-});
+})
 
 // apply dim style to verbose logs
-verbOut.pipe(process.stdout);
+verbOut.pipe(process.stdout)
 
-options.stderr = process.stderr;
-options.stdout = verbOut;
-const remoteCode = new RemoteCode(options);
+options.stderr = process.stderr
+options.stdout = verbOut
+const remoteCode = new RemoteCode(options)
 
 // turn everything magenta
 function log(...data) {
-	console.log(chalk.magenta(...data));
+	console.log(chalk.magenta(...data))
 }
 
 // add console output for livefeed (process on the remote)
-const liveSsh = remoteCode.ssh.liveReload;
+const liveSsh = remoteCode.ssh.liveReload
 liveSsh.getEventEmitter()
 	.on('connect', () => log('👀\tlive-feed connected..'))
 	.on('close', () => {
-		log('👀\tlive-feed closed');
+		log('👀\tlive-feed closed')
 		// end the node process
-		process.exit(0);
+		process.exit(0)
 	})
 	.on('error', s => console.log(chalk.bold.red(s)))
-	.on('data', data => process.stdout.write(chalk.blue(data.toString())));
+	.on('data', data => process.stdout.write(chalk.blue(data.toString())))
 
 // add generic output for the overall process of the remotecode execution
 remoteCode.emitter
@@ -124,20 +139,20 @@ remoteCode.emitter
 	.on('install', s => {
 		// do not log 'triggered' unless in verbose mode
 		if (options.verbose || s !== 'triggered') {
-			log(`📦\tdependency installation ${s}`);
+			log(`📦\tdependency installation ${s}`)
 		}
 	})
 	.on('nodemon', () => log('🔃\tnodemon process started'))
 	.on('close', () => log('🐪\tshutting down remote-code'))
 	.on('sync', () => log('✈️\tsyncing files'))
 	.on('error', e => {
-		console.log(`💣\t${chalk.red(e)}`);
+		console.log(`💣\t${chalk.red(e)}`)
 		remoteCode.close()
-		.then(() => process.exit());
-	});
+			.then(() => process.exit())
+	})
 
 // start remotecode procedure
-remoteCode.start();
+remoteCode.start()
 
 // listen for ctrl+c on terminal
-process.on('SIGINT', () => remoteCode.close());
+process.on('SIGINT', () => remoteCode.close())
--- index.js
+++ index.js
@@ -1,109 +1,127 @@
-const EventEmitter = require('events');
-const stream = require('stream');
-const Watcher = require('./lib/watcher');
-const Ssh = require('./lib/ssh');
-const Sync = require('./lib/sync');
+const EventEmitter = require('events')
+const stream = require('stream')
+const config = require('config')
+const Watcher = require('./lib/watcher')
+const Ssh = require('./lib/ssh')
+const Sync = require('./lib/sync')
 
-const devNull = new stream.Writable();
-devNull._write = () => null;
+const devNull = new stream.Writable()
+devNull._write = () => null
 
 class RemoteCode {
 	constructor(opts = {}) {
-		this.options = opts;
-		this.emitter = new EventEmitter();
+		// Note: config.get returns immutable object so need to clone to change things
+		this.options = opts.host ? opts : Object.assign({}, config.get('options'))
+		// Set defaults
+		this.options.ssh.keepaliveInterval = opts.ssh.keepaliveInterval || 500
+		this.options.ssh.readyTimeout = opts.ssh.readyTimeout || 2000
+		this.options.ssh.port = opts.ssh.port || 22
+		this.options.source = opts.source || '.'
+		this.options.install = opts.install || 'yarn'
+		this.options.install = opts.registry ? `${this.options.install} --registry ${opts.registry}` : this.options.install
+		this.options.start = opts.start || 'nodemon .'
+		if (!(opts.ssh.keyfilePath || opts.ssh.password)) {
+			this.options.ssh.agent = process.env[opts.ssh.agent] || process.env.SSH_AUTH_SOCK || process.env.SSH_AGENT_SOCK
+			if (!this.options.ssh.agent) {
+				return new Error('no ssh authentification method provided')
+			}
+		}
+		this.emitter = new EventEmitter()
 		this.ssh = {
 			liveReload: new Ssh()
-		};
-		this.verbose = opts.verbose || false;
-		this.stdout = opts.stdout instanceof stream.Writable ? opts.stdout : new stream.Writable();
-		this.stderr = opts.stderr instanceof stream.Writable ? opts.stderr : new stream.Writable();
-		this.watcher = new Watcher(opts);
-		this.sync = new Sync(opts)
+		}
+		this.verbose = opts.verbose || false
+		this.stdout = opts.stdout instanceof stream.Writable ? opts.stdout : new stream.Writable()
+		this.stderr = opts.stderr instanceof stream.Writable ? opts.stderr : new stream.Writable()
+		this.watcher = new Watcher(this.options)
+		this.sync = new Sync(this.options)
 			.addStdOutStream(this.stdout)
-			.addStdErrStream(this.stderr);
+			.addStdErrStream(this.stderr)
 
-		return this;
+		return this
 	}
 
 	syncCode() {
-		this.emitter.emit('sync');
-		return this.sync.execute();
+		this.emitter.emit('sync')
+		return this.sync.execute()
 	}
 
 	_getStdOut() {
 		if (this.verbose) {
-			return this.stdout;
+			return this.stdout
 		}
-		return devNull;
+		return devNull
 	}
 
 	_getStdErr() {
 		if (this.verbose) {
-			return this.stderr;
+			return this.stderr
 		}
-		return devNull;
+		return devNull
 	}
 
 	watch() {
-		this.watcher.start();
-		const watchEmitter = this.watcher.getEventEmitter();
+		this.watcher.start()
+		const watchEmitter = this.watcher.getEventEmitter()
 		watchEmitter.on('sync', () => {
-			this.syncCode();
-		});
+			this.syncCode()
+		})
 		watchEmitter.on('install', () => {
 			return this.install()
-			.then(() => {
-				this.ssh.liveReload.send('rs');
-			});
-		});
-		return this.emitter;
+				.then(() => {
+					this.ssh.liveReload.send('rs')
+				})
+		})
+		return this.emitter
 	}
 
 	start() {
-		this.emitter.emit('start');
-		const sshSettings = this.options.ssh;
+		this.emitter.emit('start')
+		const sshSettings = this.options.ssh
 		return Promise.all([this.syncCode(), this.watch()])
 			.then(() => this.install())
 			.then(() => this.ssh.liveReload.connect(sshSettings))
 			.then(() => {
-				this.emitter.emit('nodemon', 'start');
-				this.ssh.liveReload.send(`cd ${this.options.target} && ${this.options.start}`);
+				this.emitter.emit('nodemon', 'start')
+				this.ssh.liveReload.send(`cd ${this.options.target} && ${this.options.start}`)
 			})
-			.catch(this._abort.bind(this));
+			.catch(this._abort.bind(this))
 	}
 
 	// execute a single command and then resolve
 	execute(cmd, stdout, stderr) {
-		this.emitter.emit('exec', cmd);
-		const ssh = new Ssh();
-		return ssh.exec(this.options.ssh, cmd, stdout, stderr);
+		this.emitter.emit('exec', cmd)
+		const ssh = new Ssh()
+		const result = ssh.exec(this.options.ssh, cmd, stdout, stderr)
+		return result
 	}
 
 	install() {
-		this.emitter.emit('install', 'triggered');
+		this.emitter.emit('install', 'triggered')
 		if (!this.installInProgress) {
-			this.emitter.emit('install', 'started');
-			this.installInProgress = true;
+			this.emitter.emit('install', 'started')
+			this.installInProgress = true
+			console.log('starting install', this.options.install)
+			// TODO: Need to set a timeout on this process in case it hangs
 			return this.execute(`cd ${this.options.target} && ${this.options.install}`, this._getStdOut(), this._getStdErr())
-			.then(res => {
-				this.emitter.emit('install', 'ended', res);
-				this.installInProgress = false;
-				// console.log(res);
-				return res;
-			});
+				.then(res => {
+					this.emitter.emit('install', 'ended', res)
+					this.installInProgress = false
+					return res
+				})
 		}
-		return Promise.resolve();
+		return Promise.resolve()
 	}
 
 	close() {
-		this.emitter.emit('close');
+		this.emitter.emit('close')
 		return Promise.all([this.watcher.close(),
-			this.ssh.liveReload.close()]);
+			this.ssh.liveReload.close()])
 	}
 
 	_abort(err) {
-		this.emitter.emit('error', err);
+		this.emitter.emit('error', err)
 	}
 }
 
-module.exports = RemoteCode;
+module.exports = RemoteCode
